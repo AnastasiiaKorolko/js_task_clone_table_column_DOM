d14fbc8da459d38107ba15d79cf2fe5557874dd5
Korolko Anastasiia <korolkonasta@gmail.com>
2024-07-26 17:20:07 +0300

    make build

---
+++ index.f361f703.js
@@ -0,0 +1,2 @@
+for(var e=document.getElementsByTagName("tr"),n=0;n<e.length;n++){var r=e[n].children,t=r[1].cloneNode(!0);e[n].insertBefore(t,r[r.length-1])}
+//# sourceMappingURL=index.f361f703.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,9 @@
+"use strict";
+const rows = document.getElementsByTagName("tr");
+for(let i = 0; i < rows.length; i++){
+    const cells = rows[i].children;
+    const clone = cells[1].cloneNode(true);
+    rows[i].insertBefore(clone, cells[cells.length - 1]);
+}
+
+//# sourceMappingURL=index.f75de5e1.js.map
